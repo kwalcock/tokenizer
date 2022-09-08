1f55df87515014e3102268b0424545e1671fb74e
Keith Alcock <github@keithalcock.com>
2022-09-08 14:22:09 -0700

    Print nicely

--- scalapy/src/main/scala/com/keithalcock/tokenizer/scalapy/ScalaPyTokenizerApp.scala
+++ scalapy/src/main/scala/com/keithalcock/tokenizer/scalapy/ScalaPyTokenizerApp.scala
@@ -2,6 +2,8 @@ package com.keithalcock.tokenizer.scalapy
 
 object ScalaPyTokenizerApp extends App {
   val words = Array("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
+  println(words.mkString(", "))
+
   val tokenizer = ScalaPyTokenizer("distilbert-base-cased")
   // val tokenizer = ScalaPyTokenizer("xlm-roberta-base")
 
