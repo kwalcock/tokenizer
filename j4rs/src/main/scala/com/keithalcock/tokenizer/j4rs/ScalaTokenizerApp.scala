efbf5eded52a32389f39829cbb8636a7ea274029
Keith Alcock <github@keithalcock.com>
2022-09-21 17:05:52 -0700

    Check that both tokenizers can be used

--- j4rs/src/main/scala/com/keithalcock/tokenizer/j4rs/ScalaTokenizerApp.scala
+++ j4rs/src/main/scala/com/keithalcock/tokenizer/j4rs/ScalaTokenizerApp.scala
@@ -2,9 +2,13 @@ package com.keithalcock.tokenizer.j4rs
 
 object ScalaTokenizerApp extends App {
   val words = Array("EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", ".")
-//  val scalaTokenizer = ScalaTokenizer("distilbert-base-cased")
-   val scalaTokenizer = ScalaTokenizer("xlm-roberta-base")
+  val names = Array(
+    "distilbert-base-cased",
+    "xlm-roberta-base"
+  )
 
-  val tokenization = scalaTokenizer.encode(words)
-  tokenization
+  names.foreach { name =>
+    val scalaTokenizer = ScalaTokenizer(name)
+    val tokenization = scalaTokenizer.encode(words)
+  }
 }
