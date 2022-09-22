77901a7ab19ea32e0063f1a69888e26da59a215b
Keith Alcock <github@keithalcock.com>
2022-09-22 03:36:45 -0700

    Move to org.clulab packages, organize

---
+++ common/src/main/scala/org/clulab/transformers/tokenizer/LibraryLoader.scala
@@ -0,0 +1,96 @@
+package org.clulab.transformers.tokenizer
+
+import java.io.{FileInputStream, FileOutputStream}
+
+object LibraryLoader {
+  val label = "[Tokenizer] "
+
+  import java.io.File
+
+  def println(message: String): Unit = {
+    System.err.print(label)
+    System.err.println(message)
+  }
+
+  def loadFromFile(pathname: String): Boolean = {
+    try {
+      System.load(pathname)
+      println("Loading rust interface from " + pathname + "...")
+      true
+    }
+    catch {
+      case _: UnsatisfiedLinkError => false
+    }
+  }
+
+  def loadFromFile(dir: String, libname: String): Boolean = {
+    val pathname = dir + File.separator + libname
+    loadFromFile(pathname)
+  }
+
+  def replaceSuffix(text: String, prev: String, next: String): String =
+    if (text.endsWith(prev)) text.substring(0, text.length - prev.length) + next
+    else text
+
+  def isMac: Boolean = System.getProperty("os.name").startsWith("Mac ")
+
+  def isApple: Boolean = System.getProperty("os.arch") == "aarch64"
+
+  def load(name: String): Unit = {
+    val libname = System.mapLibraryName(name);
+    // The Mac reports a libname ending with .dylib, but Java needs .jnilib instead.
+    val jniname = replaceSuffix(libname, ".dylib", ".jnilib");
+    val resourcename =
+      if (!isMac) jniname
+      else if (isApple) "apple-" + jniname
+      else "intel-" + jniname
+    val loadedFromPwd = {
+      // Try current directory first.
+      val location = System.getProperty("user.dir")
+      println("Checking " + location + " for " + jniname + "...")
+      loadFromFile(System.getProperty("user.dir"), jniname)
+    }
+    val loadedFromHome = !loadedFromPwd && {
+      // Try home directory next.
+      val location = System.getProperty("user.home")
+      println("Checking " + location + " for " + jniname + "...")
+      loadFromFile(location, jniname);
+    }
+    val loadedFromResource = !loadedFromPwd && !loadedFromHome && {
+      // Attempt to load from the resource via the tmp directory.
+      val index = jniname.indexOf('.')
+      val prefix = jniname.substring(0, index)
+      val suffix = jniname.substring(index)
+      val tempFile = File.createTempFile(prefix + "-", suffix)
+      println("Extracting resource " + resourcename + " to " + tempFile.getAbsolutePath + "...")
+
+      // Load the jnilib from the JAR-ed resource file, and write it to the temp file.
+      // We've anticipated the name and used it for the resource, but that could go wrong.
+      val inputStream = getClass.getClassLoader.getResourceAsStream(resourcename)
+      val outputStream = new FileOutputStream(tempFile)
+
+      val buf = new Array[Byte](8192)
+      while ( {
+        val len = inputStream.read(buf)
+        if (len > 0) {
+          outputStream.write(buf, 0, len)
+          true
+        }
+        else false
+      }) {}
+
+      outputStream.flush()
+      // Prevent let the OS from removing the temporary file.
+      val lock = new FileInputStream(tempFile)
+      outputStream.close()
+
+      val loaded = loadFromFile(tempFile.getAbsolutePath)
+      lock.close()
+      tempFile.delete
+      loaded
+    }
+
+    if (!(loadedFromPwd || loadedFromHome || loadedFromResource))
+      throw new RuntimeException("j4rs could not be loaded!")
+  }
+}
--- j4rs/src/main/rust/src/rust_tokenizer.rs
+++ j4rs/src/main/rust/src/rust_tokenizer.rs
@@ -17,12 +17,11 @@ lazy_static! {
     };
 }
 
-// j4rs insists that a Result is always returned and it must be an Instance.
-#[call_from_java("com.keithalcock.tokenizer.j4rs.JavaTokenizer.create")]
+#[call_from_java("org.clulab.transformers.tokenizer.j4rs.JavaJ4rsTokenizer.create")]
 fn create_rust_tokenizer(name_instance: Instance) -> Result<Instance, String> {
     let jvm: Jvm = Jvm::attach_thread().unwrap();
     let name: String = jvm.to_rust(name_instance).unwrap();
-    println!("create_rust_tokenizer({})", name);
+    println!("create_rust_tokenizer(\"{}\")", name);
 
     let tokenizer = Tokenizer::from_pretrained(name, None).unwrap();
     let tokenizer_id = unsafe {
@@ -37,26 +36,22 @@ fn create_rust_tokenizer(name_instance: Instance) -> Result<Instance, String> {
     return tokenizer_id_result;
 }
 
-#[call_from_java("com.keithalcock.tokenizer.j4rs.JavaTokenizer.destroy")]
+#[call_from_java("org.clulab.transformers.tokenizer.j4rs.JavaJ4rsTokenizer.destroy")]
 fn destroy_rust_tokenizer(tokenizer_id_instance: Instance) {
     let jvm: Jvm = Jvm::attach_thread().unwrap();
     let tokenizer_id: i32 = jvm.to_rust(tokenizer_id_instance).unwrap();
     println!("destroy_rust_tokenizer({})", tokenizer_id);
+
     TOKENIZER_MAP.lock().unwrap().remove(&tokenizer_id);
     return;
 }
 
-#[call_from_java("com.keithalcock.tokenizer.j4rs.JavaTokenizer.tokenize")]
+#[call_from_java("org.clulab.transformers.tokenizer.j4rs.JavaJ4rsTokenizer.tokenize")]
 fn rust_tokenizer_tokenize(tokenizer_id_instance: Instance, words_instance: Instance) -> Result<Instance, String> {
     let jvm: Jvm = Jvm::attach_thread().unwrap();
     let tokenizer_id: i32 = jvm.to_rust(tokenizer_id_instance).unwrap();
     let words: Vec<String> = jvm.to_rust(words_instance).unwrap();
     println!("rust_tokenizer_tokenize({}, <words>)", tokenizer_id);
-    for word in &words {
-        print!("{} ", word);
-    }
-    println!("");
-    println!("No more words");
 
     let encoding = {
         let tokenizer_mutex = TOKENIZER_MAP.lock().unwrap();
@@ -82,18 +77,11 @@ fn rust_tokenizer_tokenize(tokenizer_id_instance: Instance, words_instance: Inst
        })
        .collect::<Vec<_>>()[..];
     let tokens = encoding.get_tokens();
-
-    for token in tokens {
-        print!("{} ", token);
-    }
-    println!();
-
-    let java_tokenization_instance = jvm.create_instance("com.keithalcock.tokenizer.j4rs.Tokenization", &[
+    let java_tokenization_instance = jvm.create_instance("org.clulab.transformers.tokenizer.j4rs.JavaJ4rsTokenization", &[
         InvocationArg::try_from(token_id_i32s).unwrap(),
         InvocationArg::try_from(word_id_i32s).unwrap(),
         InvocationArg::try_from(tokens).unwrap()
     ]).unwrap();
-    let java_tokenization_result = Instance::try_from(java_tokenization_instance).map_err(|error| format!("{}", error));
 
-    return java_tokenization_result;
+    return Ok(java_tokenization_instance);
 }
