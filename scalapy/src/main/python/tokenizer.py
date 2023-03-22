81169cdcaf4bc383f4c2d786d9dbdf7dbd661ffc
Keith Alcock <github@keithalcock.com>
2023-03-21 19:45:54 -0700

    Switch spaces on and off

--- scalapy/src/main/python/tokenizer.py
+++ scalapy/src/main/python/tokenizer.py
@@ -3,25 +3,31 @@ from transformers import AutoTokenizer
 
 def procedurally():
   words = ["EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", "."]
-  names = [
-    "distilbert-base-cased",
-    "xlm-roberta-base",
-    "bert-base-cased"
+  name_and_space_tuples = [
+    ("distilbert-base-cased", False),
+    ("xlm-roberta-base", False),
+    ("bert-base-cased", False),
+    ("google/bert_uncased_L-4_H-512_A-8", False),
+    ("roberta-base", True)
+    # "microsoft/deberta-v3-base"
   ]
 
-  for name in names:
-    tokenizer = AutoTokenizer.from_pretrained(name)
+  print("Procedurally...")
+  for name, space in name_and_space_tuples:
+    tokenizer = AutoTokenizer.from_pretrained(name, add_prefix_space=space)
     token_input = tokenizer(words, is_split_into_words = True)
     token_ids = token_input['input_ids']
-    print(token_ids)
     word_ids = token_input.word_ids(batch_index = 0)
-    print(word_ids)
     tokens = token_input.tokens(batch_index = 0)
+    print(name)
+    print(token_ids)
+    print(word_ids)
     print(tokens)
+    print()
 
 class Tokenizer():
-  def __init__(self, name: str):
-    self.tokenizer = AutoTokenizer.from_pretrained(name)
+  def __init__(self, name: str, add_prefix_space: bool):
+    self.tokenizer = AutoTokenizer.from_pretrained(name, add_prefix_space=add_prefix_space)
 
   def tokenize(self, words: list[str]) -> tuple[list[int], list[int]]:
     token_input = self.tokenizer(words, is_split_into_words = True)
@@ -32,18 +38,24 @@ class Tokenizer():
 
 def objectively():
   words = ["EU", "rejects", "German", "call", "to", "boycott", "British", "lamb", "."]
-  names = [
-    "distilbert-base-cased",
-    "xlm-roberta-base",
-    "bert-base-cased"
+  name_and_space_tuples = [
+    ("distilbert-base-cased", False),
+    ("xlm-roberta-base", False),
+    ("bert-base-cased", False),
+    ("google/bert_uncased_L-4_H-512_A-8", False),
+    ("roberta-base", True)
+    # "microsoft/deberta-v3-base"
   ]
 
-  for name in names:
-    tokenizer = Tokenizer(name) # add something here for spaces
+  print("Objectively...")
+  for name, space in name_and_space_tuples:
+    tokenizer = Tokenizer(name, add_prefix_space=space)
     token_ids, word_ids, tokens = tokenizer.tokenize(words)
+    print(name)
     print(token_ids)
     print(word_ids)
     print(tokens)
+    print()
 
 procedurally()
 objectively()
